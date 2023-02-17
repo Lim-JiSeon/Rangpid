5701c41cb990e4ae7f65b37937dd4ec239d2d6c6
Lim-JiSeon <83554018+Lim-JiSeon@users.noreply.github.com>
2023-02-17 22:33:19 +0900

    Delete Rangpid directory

--- Rangpid/backend/config.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-    DB_URL: process.env.DB_URL || 'mongodb://127.0.0.1:27017/rangpid',
-    JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
-}
